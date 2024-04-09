cb61832455f1103a4d3190086457841f99209622
arkregiel <arkregiel@gmail.com>
2024-04-10 00:33:57 +0200

    refactor partition

---
+++ cmd/edgecases.go
@@ -0,0 +1,26 @@
+package cmd
+
+type EdgeCaseResult struct {
+  Algorithm string
+  AvgTime  float64
+}
+
+func TestEdgeCases() {
+  
+}
+
+func TestFullRandom() {
+
+}
+
+func TestSortedAscending() {
+
+}
+
+func TestSortedDescending() {
+
+}
+
+func TestPartiallySorted() {
+
+}
--- sort/quicksort.go
+++ sort/quicksort.go
@@ -21,39 +21,37 @@ func QuickSort[T constraints.Ordered](array []T, low, high int, getPivot PivotIn
 
 func partition[T constraints.Ordered](array []T, low, high int, getPivot PivotIndexChoosingMethod[T]) int {
   pivotIndex := getPivot(array, low, high)
-  array[high], array[pivotIndex] = array[pivotIndex], array[high]
-  pivot := array[high]
+  array[low], array[pivotIndex] = array[pivotIndex], array[low]
+  pivot := array[low]
 
-  i := low - 1
-	for j := low; j < high; j++ {
-		if array[j] <= pivot {
-			i++
-			array[i], array[j] = array[j], array[i]
-		}
-	}
-	i++
-	array[i], array[high] = array[high], array[i]
-	
-  return i
+  m := low
+  for i := low + 1; i <= high; i++ {
+    if array[i] < pivot {
+      m++
+      array[m], array[i] = array[i], array[m]
+    }
+  }
+  array[low], array[m] = array[m], array[low]
+  return m
 }
 
 // GetPivotLow wybiera jako oś lewy element z danego zakresu
-func GetPivotLow[T constraints.Ordered](array []T, low, _ int) int {
+func GetPivotLow[T constraints.Ordered](_ []T, low, _ int) int {
   return low
 }
 
 // GetPivotHigh wybiera jako oś prawy element z danego zakresu
-func GetPivotHigh[T constraints.Ordered](array []T, _, high int) int {
+func GetPivotHigh[T constraints.Ordered](_ []T, _, high int) int {
   return high
 }
 
 // GetPivotMiddle wybiera jako oś środkowy element z danego zakresu
-func GetPivotMiddle[T constraints.Ordered](array []T, low, high int) int {
+func GetPivotMiddle[T constraints.Ordered](_ []T, low, high int) int {
   return (low + high) / 2
 }
 
 // GetPivotRandom wbyiera jako oś losowy element z danego zakresu
-func GetPivotRandom[T constraints.Ordered](array []T, low, high int) int {
+func GetPivotRandom[T constraints.Ordered](_ []T, low, high int) int {
   return rand.Intn(high - low + 1) + low
 }
 
